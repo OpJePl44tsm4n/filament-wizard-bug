364c4569592f70a2326092143f0ba9e57940fe58
PimPlaatsman <pim.plaatsman@gmail.com>
2024-08-05 13:27:53 +0200

    checkbox inside modal wizard

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\Wizard\Step;
+use Illuminate\Support\HtmlString;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required(),
+                Forms\Components\DateTimePicker::make('email_verified_at'),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\Action::make('wizard')
+                    ->steps([
+                        Step::make('Name')
+                            ->description('Give the category a unique name')
+                            ->schema([
+                                TextInput::make('name')
+                                    ->required()
+                                    ->live()
+                                    ->afterStateUpdated(fn ($state, callable $set) => $set('slug', $state)),
+                                TextInput::make('slug')
+                                    ->disabled()
+                                    ->required(),
+                                Checkbox::make('checkbox')
+                                    ->label('required Checkbox')
+                                    ->required(),
+                            ])
+                            ->columns(2),
+                        Step::make('Visibility')
+                            ->description('Control who can view it')
+                            ->schema([
+                                TextInput::make('alphaNum')
+                                    ->label('Alpha Numeric / required test')
+                                    ->required()
+                                    ->alphaNum(),
+                                Checkbox::make('terms')
+                                    ->label('Accept terms and conditions (is not checked for required validation)')
+                                    ->required()
+                            ]),
+                    ])
+
+
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
